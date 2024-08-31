dee1f92f2513ef36453ce696260926953259a4e8
delfoxs <140474808+delfoxs@users.noreply.github.com>
2024-08-31 19:23:30 +0930

    Create RecursiveBinarySearch.h

---
+++ RecursiveBinarySearch.h
@@ -0,0 +1,9 @@
+#ifndef RECURSIVEBINARYSEARCH_H
+#define RECURSIVEBINARYSEARCH_H
+
+class RecursiveBinarySearch {
+public:
+    static int binarySearch(int list[], int start, int end, int target);
+};
+
+#endif
